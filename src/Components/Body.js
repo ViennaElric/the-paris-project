2d2cb1b952a6c9c85bd0f5044c2b7da5c6186cc7
vienna <viennaelric@gmail.com>
2021-12-01 20:20:57 -0500

    added body, leftcolumn, rightcolumn, and footer componenets

---
+++ src/Components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LeftColumn from "./LeftColumn";
+import RightColumn from "./RightColumn";
+
+const Body = () => (
+    <div>
+        <LeftColumn />
+        <RightColumn />
+    </div>
+);
+
+export default Body;
